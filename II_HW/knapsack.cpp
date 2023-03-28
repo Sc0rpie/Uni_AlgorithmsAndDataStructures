e09beba1c3387c4907494008397974b94316f6dc
Olen <olen2003@gmail.com>
2023-03-28 12:36:58 +0300

    Removed unneeded code, added comments.

--- II_HW/knapsack.cpp
+++ II_HW/knapsack.cpp
@@ -20,18 +20,16 @@ void printKnapsack(vector<Item> items, vector<int> knapsack, int option, int ind
     int totalPrice = 0;
     int totalWeight = 0;
     int size = knapsack.size() - count(knapsack.begin(), knapsack.end(), 0);
+
     if (option == 0)
         size++;
     for (int i = 0; i < size-1; i++)
-    {
-        // cout << '-';
         outFull << '-';
-    }
-    // if (addedElement != -1)
-        outFull << "Bandoma prideti d" << addedElement+1 << ". Kuprine = "; 
-    // cout << "{ ";
+
+    outFull << "Bandoma prideti d" << addedElement+1 << ". Kuprine = "; 
     outFull << "{";
 
+    // Printing items in the knapsack (Kuprine = {...})
     int counter = 0;
     for (int i = 0; i < knapsack.size(); i++)
     {
@@ -45,17 +43,16 @@ void printKnapsack(vector<Item> items, vector<int> knapsack, int option, int ind
             counter++;
         }
     }
+    // Printing the last item in the knapsack if backtracking
     if (option == 0)
     {
-        // cout << "D" << index << " ";
         outFull << " d" << index;
         totalPrice += items[index - 1].value;
         totalWeight += items[index - 1].weight;
     }
-
-    // cout << "} ";
     outFull << "}. ";
-    // cout << "K: " << totalPrice << " ";
+
+    // Printing th total price of the knapsack (Kaina = ...+...+...=...)
     outFull << "Kaina = ";
     counter = 0;
     for (int i = 0; i < knapsack.size(); i++)
@@ -68,12 +65,19 @@ void printKnapsack(vector<Item> items, vector<int> knapsack, int option, int ind
             counter++;
         }
     }
+    // If backtracking (option == 0), print the last item as well
+    if (option == 0)
+    {
+        outFull << '+' << items[index - 1].value;
+    }
+    // If there is only one item in the knapsack, print a dot instead of the equal sign
+    // else print the equal sign and the total price
     if (counter == 1)
         outFull << ". ";
     else
         outFull << "=" << totalPrice << ". ";
     
-    // cout << "S: " << totalWeight;
+    // Printing the total weight of the knapsack (Svoris = ...+...+...=...)
     outFull << "Svoris = ";
     counter = 0;
     for (int i = 0; i < knapsack.size(); i++)
@@ -86,42 +90,44 @@ void printKnapsack(vector<Item> items, vector<int> knapsack, int option, int ind
             counter++;
         }
     }
+    // If backtracking (option == 0), print the last item as well
+    if (option == 0)
+    {
+        outFull << '+' << items[index - 1].weight;
+    }
+    // If there is only one item in the knapsack, print a dot instead of the equal sign
+    // else print the equal sign and the total weight
     if (counter == 1)
         outFull << ". ";
     else
         outFull << "=" << totalWeight << ". ";
 
+    // If its going deeper in the tree and last knapsack item is not added, print "GILYN"
     if (option && knapsack[knapsack.size() - 1] != 1)
     {
-        // cout << " - GILYN";
         outFull << " GILYN.";
     }
     else
     {
+        // If the total weight of the knapsack is greater than the capacity of the knapsack, print "BACKTRACK"
         if (totalWeight > S)
             outFull << " BACKTRACK, nes svoris virsija maksimuma S = " << S << '.';
-        // cout << " - BACKTRACK";
         else
             outFull << " BACKTRACK, parenkama kita kombinacija.";
     }
-    // cout << endl;
     outFull << endl;
 }
 
-void printAnswer(vector<Item> items, vector<int> knapsack, ofstream &outShort, ofstream &outFull)
+void printAnswer(vector<Item> items, vector<int> knapsack, ofstream &outFull)
 {
     int totalPrice = 0;
     int totalWeight = 0;
 
-    // cout << endl << "\t\t{ ";
-    // outShort << endl << "\t\t{ ";
     outFull << endl << "\t\t{";
     for (int i = 0; i < knapsack.size(); i++)
     {
         if (knapsack[i] == 1)
         {
-            // cout << "D" << i + 1 << " ";
-            // outShort << "D" << i + 1 << " ";
             if (i == knapsack.size()-1)
                 outFull << "d" << i + 1;
             else
@@ -131,20 +137,9 @@ void printAnswer(vector<Item> items, vector<int> knapsack, ofstream &outShort, o
         }
     }
 
-    // cout << "} " << endl;
-    // outShort << "} " << endl;
     outFull << "}. " << endl;
-
-    // cout << "\t\tKaina: " << totalPrice << endl;
-    // outShort << "\t\tKaina: " << totalPrice << endl;
     outFull << "\t\tKaina = " << totalPrice << '.' << endl;
-
-    // cout << "\t\tSvoris: " << totalWeight;
-    // outShort << "\t\tSvoris: " << totalWeight;
     outFull << "\t\tSvoris = " << totalWeight << '.';
-
-    // cout << endl;
-    // outShort << endl;
     outFull << endl;
 }
 
@@ -184,12 +179,22 @@ void knapsackBacktracking(vector<Item> items, int capacity, vector<int> knapsack
     // Check if adding the current item to the knapsack is feasible
     if (items[index].weight <= capacity)
     {
+        // Print the current step
         outFull << setw(5) << steps++ << ") ";
+
+        // Current item is being added into a knapsack
         knapsack[index] = 1;
-        // cout << "INDEX: " << index << endl;
+
+        // Print the current knapsack
         printKnapsack(items, knapsack, 1, 0, outFull, index);
+
+        // Recursively call the function to add the next item
         knapsackBacktracking(items, capacity - items[index].weight, knapsack, index + 1, bestKnapsack, bestValue, outFull);
+
+        // Boolean for removing repeated printKnapsack calls
         after = true;
+
+        // Current item is being removed from the knapsack
         knapsack[index] = 0;
     }
 
@@ -204,18 +209,10 @@ void knapsackBacktracking(vector<Item> items, int capacity, vector<int> knapsack
 
 int main()
 {
-
-    // cout << "Olen Rackauskas, 1 kursas, 3 grupe, 2 pogrupis, 2 uzduotis, 3 variantas" << endl
-        // << endl;
-    // cout << "Uzduoties aprasas: Duota N daiktu, kuriu svoriai s1, s2, ... sN, o kainos k1, k2, ... kN. Programa turi sudaryti daiktu rinkini, kurio kaina butu maksimali, o svoris nevirsytu nurodyto svorio S. Vartotojas nurodo faila, is kurio programa iveda daiktu svorius ir kainas, bei svori S." << endl
-        // << endl;
-
     string filename;
     cout << "Iveskite failo pavadinima (rasoma su pletiniu gale): ";
-    getline(cin, filename);
+    getline(cin, filename);               // get filename from user
     ifstream src(filename.c_str());       // open data file
-    ofstream outShort("out-trumpas.txt"); // open short output file
-    ofstream outFull("out-ilgas.txt");    // open full output file
     if (src)
     {
         cout << "Failas " << filename << " nuskaitytas sekmingai." << endl << endl;
@@ -225,22 +222,16 @@ int main()
         cout << "Failas " << filename << " nerastas." << endl << endl;
         return 0;
     }
+    // Open output file after checking if the input file exists
+    ofstream outFull("out-ilgas.txt");    // open full output file
 
-    // outShort << "Olen Rackauskas, 1 kursas, 3 grupe, 2 pogrupis, 2 uzduotis, 3 variantas" << endl
-        // << endl;
-    outFull << "Olen Rackauskas, 1 kursas, 3 grupe, 2 pogrupis." << endl
-        << endl;
-    // 2 uzduotis, 3 variantas
-    // outShort << "Uzduoties aprasas: Duota N daiktu, kuriu svoriai s1, s2, ... sN, o kainos k1, k2, ... kN. Programa turi sudaryti daiktu rinkini, kurio kaina butu maksimali, o svoris nevirsytu nurodyto svorio S. Vartotojas nurodo faila, is kurio programa iveda daiktu svorius ir kainas, bei svori S." << endl
-        // << endl;
 
-    // cout << "Ivesties failas: " << filename << endl << endl;
-    // outShort << "Ivesties failas: " << filename << endl << endl;
+    // Print the header of the output file
+    outFull << "Olen Rackauskas, 1 kursas, 3 grupe, 2 pogrupis." << endl << endl;
     outFull << "1 DALIS. DUOMENYS." << endl;
     outFull << "2 uzduotis, 3 variantas" << endl << endl;
     outFull << "1.1. SALYGA. ";
-    outFull << "Duota N daiktu, kuriu svoriai s1, s2, ... sN, o kainos k1, k2, ... kN. Programa turi sudaryti daiktu rinkini, kurio kaina butu maksimali, o svoris nevirsytu nurodyto svorio S. Vartotojas nurodo faila, is kurio programa iveda daiktu svorius ir kainas, bei svori S." << endl
-        << endl;
+    outFull << "Duota N daiktu, kuriu svoriai s1, s2, ... sN, o kainos k1, k2, ... kN. Programa turi sudaryti daiktu rinkini, kurio kaina butu maksimali, o svoris nevirsytu nurodyto svorio S. Vartotojas nurodo faila, is kurio programa iveda daiktu svorius ir kainas, bei svori S." << endl << endl;
     outFull << "1.2. PRADINE BUSENA." << endl;
     outFull << "\t1.2.1)Ivesties failas: " << filename << endl;
 
@@ -257,42 +248,24 @@ int main()
     }
     src.close();
 
-    // vector<Item> items = {{1,5}, {2,6}, {3,7}, {4,8}, {5,9}};
-    // cout << "I DALIS: Duomenys" << endl;
-    // outShort << "I DALIS: Duomenys" << endl;
-    
-
-    // cout << "\t1)Daiktu skaicius N: " << N << endl;
-    // outShort << "\t1)Daiktu skaicius N: " << N << endl;
     outFull << "\t1.2.2)Daiktu skaicius N: " << N << endl;
-
-    // cout << "\t2)Maksimalus daiktu svoris S: " << S << endl;
-    // outShort << "\t2)Maksimalus daiktu svoris S: " << S << endl;
     outFull << "\t1.2.3)Maksimalus daiktu svoris S: " << S << endl;
-
-    // cout << "\t3)Daiktu svoriai ir kainos:" << endl;
-    // outShort << "\t3)Daiktu svoriai ir kainos:" << endl;
     outFull << "\t1.2.4)Daiktu svoriai ir kainos:" << endl;
 
+    // Print out the items (d1. s1 = ... k1 = ...)
     for (int i = 0; i < N; i++)
-    {
-        // cout << "\t\tD" << i + 1 << ": svoris - " << items[i].weight << "; kaina - " << items[i].value << endl;
-        // outShort << "\t\tD" << i + 1 << ": svoris - " << items[i].weight << "; kaina - " << items[i].value << endl;
         outFull << "\t\td" << i + 1 << ". s" << i+1 << " = " << items[i].weight << ". k" << i+1 << " = " << items[i].value << '.' << endl;
-    }
-    // cout << endl;
-    // outShort << endl;
+
     outFull << endl;
+
+    // Initialize vectors with size N, filled with 0
     vector<int> knapsack(items.size(), 0);
     vector<int> bestKnapsack(items.size(), 0);
     int bestValue = 0;
 
-    // cout << "II DALIS:" << endl;
     outFull << "2 DALIS. VYKDYMAS." << endl;
     knapsackBacktracking(items, S, knapsack, 0, bestKnapsack, bestValue, outFull);
 
-    // cout << endl << "III DALIS: Rezultatas" << endl;
-    // outShort << endl << "III DALIS: Rezultatas" << endl;
     outFull << endl << "3 DALIS. Rezultatas" << endl;
 
     int itemCount = 0;
@@ -304,23 +277,15 @@ int main()
 
     if(itemCount > 0)
     {
-        // cout << "\t3.1)Daiktu sarasas rastas. Bandymu skaicius: " << steps - 1 << endl;
-        // outShort << "\t3.1)Daiktu sarasas rastas. Bandymu skaicius: " << steps - 1 << endl;
         outFull << "\t3.1)Daiktu sarasas rastas. Bandymu skaicius: " << steps - 1 << endl;
-
-        // cout << "\t3.2)Geriausias rastas daiktu rinkinys: ";
-        // outShort << "\t3.2)Geriausias rastas daiktu rinkinys: ";
         outFull << "\t3.2)Geriausias rastas daiktu rinkinys: ";
-        printAnswer(items, bestKnapsack, outShort, outFull);
+        printAnswer(items, bestKnapsack, outFull);
     }
     else
     {
-        // cout << "\t3.1)Daiktu sarasas nerastas. Bandymu skaicius: " << steps - 1 << endl << "\tNera daiktu, kurie turetu svori mazesni nei " << S << endl;
-        // outShort << "\t3.1)Daiktu sarasas nerastas. Bandymu skaicius: " << steps - 1 << endl << "\tNera daiktu, kurie turetu svori mazesni nei " << S << endl;
         outFull << "\t3.1)Daiktu sarasas nerastas. Bandymu skaicius: " << steps - 1 << endl << "\tNera daiktu, kurie turetu svori mazesni nei " << S << endl;
     }
 
-    outShort.close();
     outFull.close();
     return 0;
 }
